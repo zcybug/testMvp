8c02871cc0b63e6cc004f96560dc45b222b90429
买个车 <839530047@qq.com>
2017-08-10 08:36:06 +0800

    test

--- kotlinlib/src/main/java/lib/android/com/kotlinlib/MainKotlinActivity.kt
+++ kotlinlib/src/main/java/lib/android/com/kotlinlib/MainKotlinActivity.kt
@@ -8,5 +8,6 @@ class MainKotlinActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_kotlin)
+        val name="我是一个TextV"
     }
 }
